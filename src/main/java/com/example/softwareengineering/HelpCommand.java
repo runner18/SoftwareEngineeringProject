cf74025309dfcbc7a60629a22b0d79d1a4dbdb71
a-duke <57417219+a-duke@users.noreply.github.com>
2021-11-01 22:59:03 -0400

    Create HelpCommand.java

---
+++ src/main/java/com/example/softwareengineering/HelpCommand.java
@@ -0,0 +1,19 @@
+package com.example.softwareengineering;
+
+import java.awt.Desktop;
+import java.net.URI;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class HelpCommand extends Command {
+
+    public HelpCommand() {}
+    public void desktopIntegration() {
+        Desktop d = Desktop.getDesktop();
+        try {
+            d.browse(new URI("https://alexduke.net/instructions_hidden"));
+        } catch (IOException | URISyntaxException e2) {
+            e2.printStackTrace();
+        }
+    }
+}
