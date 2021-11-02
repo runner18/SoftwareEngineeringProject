1895842d50f3f035bf5dea2487d224667045ab7c
a-duke <57417219+a-duke@users.noreply.github.com>
2021-11-01 23:00:35 -0400

    Update HelpCommand.java

--- src/main/java/com/example/softwareengineering/HelpCommand.java
+++ src/main/java/com/example/softwareengineering/HelpCommand.java
@@ -11,7 +11,7 @@ public class HelpCommand extends Command {
     public void desktopIntegration() {
         Desktop d = Desktop.getDesktop();
         try {
-            d.browse(new URI("https://alexduke.net/instructions_hidden"));
+            d.browse(new URI("https://alexduke.net/instructions_hidden.html"));
         } catch (IOException | URISyntaxException e2) {
             e2.printStackTrace();
         }
