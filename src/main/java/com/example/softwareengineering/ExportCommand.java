5eae3995e1021d654fcb758c0ca655f9f1bb1b00
a-duke <57417219+a-duke@users.noreply.github.com>
2021-11-01 22:58:28 -0400

    Create ExportCommand.java

---
+++ src/main/java/com/example/softwareengineering/ExportCommand.java
@@ -0,0 +1,11 @@
+package com.example.softwareengineering;
+
+public class ExportCommand extends Command {
+
+    public ExportCommand() {}
+
+    public void exportIntegration() {
+        System.out.println("[DEBUG: COMMAND HANDLER] Export command successfully read."); //Future implementation- waiting for Thane's DB
+        }
+
+}
