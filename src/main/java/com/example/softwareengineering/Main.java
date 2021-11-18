7210d78a9acaff28815ab6bf9e005f579c7f633e
a-duke <a-duke@onu.edu>
2021-11-18 13:37:37 -0500

    FP CCU 2.3

--- src/main/java/com/example/softwareengineering/Main.java
+++ src/main/java/com/example/softwareengineering/Main.java
@@ -1,59 +1,43 @@
+/**
+ * Softball Statistics Inventory Manager
+ * Software to calculate and crunch various softball statistics to streamline the analysis process
+ * Main.java - Main Class
+ * Course: Software Engineering
+ * Instructor: Dr. Malik
+ * Date: November 21, 2021
+ * Authors: Thane Class, Alex Diehl, Alex Duke, and Stacey Walters
+ */
+
 package com.example.softwareengineering;
 
 import javafx.application.Application;
-import javafx.beans.InvalidationListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.util.Callback;
-
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("interface.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 290);
+        Scene scene = new Scene(fxmlLoader.load(), 801, 290);
         stage.setTitle("Softball Statistics Inventory Manager");
         stage.getIcons().add(new Image("https://www.nicepng.com/png/full/40-407156_clip-art-library-stock-collection-of-softball-images.png"));
         stage.setScene(scene);
         stage.show();
-        ComboBox btnInsertTeam = (ComboBox) scene.lookup("#btnInsertTeam");
-        ToggleButton btnInsertData = (ToggleButton) scene.lookup("#btnInsertData");
+
+        Command cmd = new Command();    //Initializing the Command interface
+        String[] pitcher = {"IP", "W", "L", "CG", "R", "ER", "B13", "K", "HR", "H"};    //Array of pitcher stats
+        String[] hitter = {"AB", "H", "1B", "2B", "3B", "HR", "BB", "K", "HBP", ""};;   //Array of hitter stats
+
+        ToggleButton btnInsertData = (ToggleButton) scene.lookup("#btnInsertData"); //UI elements
         Button btnHelp = (Button) scene.lookup("#btnHelp");
-        ToggleButton btnCompare = (ToggleButton) scene.lookup("#btnCompare");/*
-        TextField txtInsertName = (TextField) scene.lookup("#txtInsertName");
-        TextField txtInsertStatOne = (TextField) scene.lookup("#txtInsertStatOne");
-        TextField txtInsertStatTwo = (TextField) scene.lookup("#txtInsertStatTwo");
-        TextField txtInsertStatThree = (TextField) scene.lookup("#txtInsertStatThree");
-        TextField txtInsertStatFour = (TextField) scene.lookup("#txtInsertStatFour");
-        TextField txtInsertStatFive = (TextField) scene.lookup("#txtInsertStatFive");
-        TextField txtInsertStatSix = (TextField) scene.lookup("#txtInsertStatSix");
-        TextField txtInsertStatSeven = (TextField) scene.lookup("#txtInsertStatSeven");
-        TextField txtInsertStatEight = (TextField) scene.lookup("#txtInsertStatEight");
-        TextField txtInsertStatNine = (TextField) scene.lookup("#txtInsertStatNine");
-        TextField txtInsertStatTen = (TextField) scene.lookup("#txtInsertStatTen");
-        ComboBox btnComparePlayerOne = (ComboBox) scene.lookup("#btnComparePlayerOne");
-        ComboBox btnComparePlayerTwo = (ComboBox) scene.lookup("#btnComparePlayerTwo");
+        ToggleButton btnCompare = (ToggleButton) scene.lookup("#btnCompare");
         ComboBox btnComparePosition = (ComboBox) scene.lookup("#btnComparePosition");
         TextField lblCompareStatOne = (TextField) scene.lookup("#lblCompareStatOne");
         TextField lblCompareStatTwo = (TextField) scene.lookup("#lblCompareStatTwo");
@@ -64,165 +48,180 @@ public class Main extends Application {
         TextField lblCompareStatSeven = (TextField) scene.lookup("#lblCompareStatSeven");
         TextField lblCompareStatEight = (TextField) scene.lookup("#lblCompareStatEight");
         TextField lblCompareStatNine = (TextField) scene.lookup("#lblCompareStatNine");
-        TextField lblCompareStatTen = (TextField) scene.lookup("#lblCompareStatTen");*/
-        TableView<PitcherModel> tblDisplayPitcher = (TableView) scene.lookup("#tblDisplayPitcher");
-
-        /*
-        ObservableList<Pitcher> pitchList = FXCollections.observableArrayList();
-        Player[] pitch = reader.getStats("p");
-        ObservableList<Hitter> hitList = FXCollections.observableArrayList();
-        Player[] hit = reader.getStats("b");
-        /*reader.setTeams(hit, pitch);
-        ObservableList<String> teamList = FXCollections.observableArrayList();
-        String[] teamsAll = reader.getTeams();
-
-        for(int i = 0; i < pitch.length; i++){
-            pitchList.add((Pitcher)pitch[i]);
-        }
-        for(int i = 0; i < hit.length; i++){
-            hitList.add((Hitter)hit[i]);
-        }/*
-        for(int i = 0; i < teamsAll.length; i++){
-            teamList.add(teamsAll[i]);
-        }
-
-         */
-        Button btnInsertSubmit = (Button) scene.lookup("#btnInsertSubmit");
+        TextField lblCompareStatTen = (TextField) scene.lookup("#lblCompareStatTen");
         ToolBar tbInsert = (ToolBar) scene.lookup("#tbInsert");
         ToolBar tbCompare = (ToolBar) scene.lookup("#tbCompare");
-
-        //TableColumn<Pitcher, String> tblDisplayPitcherName = new TableColumn<>("#tblDisplayPitcherName");
-        //tblDisplayPitcherName.setCellValueFactory(new PropertyValueFactory<>("personName"));
-
-/*        TableColumn<PitcherModel, String> tblDisplayPitcherName = new TableColumn<>("#tblDisplayPitcherName");
-
-        tblDisplayPitcherName.setCellValueFactory(new PropertyValueFactory<>("PlayerName"));
-
-        Player[] pitch = reader.getStats("p");
-        ObservableList<PitcherModel> pitchList = FXCollections.observableArrayList();
-        for(int i = 0; i < pitch.length; i++) {
-            pitchList.add(new PitcherModel((Pitcher) pitch[i]));
-        }
-        PitcherModel test = new PitcherModel((Pitcher) pitch[0]);
-        System.out.println(tblDisplayPitcherName.getCellValueFactory());
-        tblDisplayPitcher.setItems(pitchList);
-        tblDisplayPitcher.getColumns().addAll(tblDisplayPitcherName);
-
-        /*
-        TableColumn tblDisplayPitcherName = (TableColumn) scene.lookup("#tblDisplayPitcherName");
-        TableColumn tblDisplayPitcherPosition = (TableColumn) scene.lookup("#tblDisplayPitcherPosition");
-        TableColumn tblDisplayPitcherH = (TableColumn) scene.lookup("#tblDisplayPitcherH");
-        TableColumn tblDisplayPitcher2B = (TableColumn) scene.lookup("#tblDisplayPitcher2B");
-        TableColumn tblDisplayPitcher3B = (TableColumn) scene.lookup("#tblDisplayPitcher3B");
-        TableColumn tblDisplayPitcherHR = (TableColumn) scene.lookup("#tblDisplayPitcherHR");
-        TableColumn tblDisplayPitcherBB = (TableColumn) scene.lookup("#tblDisplayPitcherBB");
-        TableColumn tblDisplayPitcherK = (TableColumn) scene.lookup("#tblDisplayPitcherK");
-        TableColumn tblDisplayPitcherAVG = (TableColumn) scene.lookup("#tblDisplayPitcherAVG");
-        TableColumn tblDisplayPitcherOBP = (TableColumn) scene.lookup("#tblDisplayPitcherOBP");
-        TableColumn tblDisplayPitcherSLG = (TableColumn) scene.lookup("#tblDisplayPitcherSLG");
-        TableColumn tblDisplayPitcherOPS = (TableColumn) scene.lookup("#tblDisplayPitcherOPS");
-*/
-        /*TableView<Hitter> tblDisplayHitter = (TableView) scene.lookup("#tblDisplayPitcher");
-
-        tblDisplayHitter.setItems(hitList);*/
-
-        /*
-        TableColumn tblDisplayHitterName = (TableColumn) scene.lookup("#tblDisplayHitterName");
-        TableColumn tblDisplayHitterPosition = (TableColumn) scene.lookup("#tblDisplayHitterPosition");
-        TableColumn tblDisplayHitterIP = (TableColumn) scene.lookup("#tblDisplayHitterIP");
-        TableColumn tblDisplayHitterW = (TableColumn) scene.lookup("#tblDisplayHitterW");
-        TableColumn tblDisplayHitterL = (TableColumn) scene.lookup("#tblDisplayHitterL");
-        TableColumn tblDisplayHitterCG = (TableColumn) scene.lookup("#tblDisplayHitterCG");
-        TableColumn tblDisplayHitterR = (TableColumn) scene.lookup("#tblDisplayHitterR");
-        TableColumn tblDisplayHitterER = (TableColumn) scene.lookup("#tblDisplayHitterER");
-        TableColumn tblDisplayHitterB13 = (TableColumn) scene.lookup("#tblDisplayHitterB13");
-        TableColumn tblDisplayHitterK = (TableColumn) scene.lookup("#tblDisplayHitterK");
-        TableColumn tblDisplayHitterHR = (TableColumn) scene.lookup("#tblDisplayHitterHR");
-        TableColumn tblDisplayHitterH = (TableColumn) scene.lookup("#tblDisplayHitterH");
-        */
-
-        Command cmd = new Command();
-        ColorPickerConverter cpc = new ColorPickerConverter();
         ColorPicker btnColorPicker = (ColorPicker) scene.lookup("#btnColorPicker");
+        Tooltip TTOne = new Tooltip(hitter[0]);
+        Tooltip TTTwo = new Tooltip(hitter[1]);
+        Tooltip TTThree = new Tooltip(hitter[2]);
+        Tooltip TTFour = new Tooltip(hitter[3]);
+        Tooltip TTFive = new Tooltip(hitter[4]);
+        Tooltip TTSix = new Tooltip(hitter[5]);
+        Tooltip TTSeven = new Tooltip(hitter[6]);
+        Tooltip TTEight = new Tooltip(hitter[7]);
+        Tooltip TTNine = new Tooltip(hitter[8]);
+        Tooltip TTTen = new Tooltip(hitter[9]);
+        lblCompareStatOne.setTooltip(TTOne);
+        lblCompareStatTwo.setTooltip(TTTwo);
+        lblCompareStatThree.setTooltip(TTThree);
+        lblCompareStatFour.setTooltip(TTFour);
+        lblCompareStatFive.setTooltip(TTFive);
+        lblCompareStatSix.setTooltip(TTSix);
+        lblCompareStatSeven.setTooltip(TTSeven);
+        lblCompareStatEight.setTooltip(TTEight);
+        lblCompareStatNine.setTooltip(TTNine);
+        lblCompareStatTen.setTooltip(TTTen);
+
+        /**
+         * ColorPicker Event Handler
+         * Handles when the color picker button is clicked
+         * Input: Value of selected color from color picker
+         * Returns: nothing
+         */
         btnColorPicker.setOnAction(actionEvent -> {
+            ColorPickerConverter cpc = new ColorPickerConverter();
             scene.getRoot().setStyle("-fx-base:" + cpc.pass(btnColorPicker.getValue()));
         });
 
+        /**
+         * Help Button Event Handler
+         * Handles when the help button is clicked
+         * Input: void
+         * Returns: void
+         */
         btnHelp.setOnAction(actionEvent -> {
-            cmd.accessExternalSource("help");
+            cmd.accessExternalSource("help"); //Calls "help" command utilizing the Command interface
         });
 
+        /**
+         * Compare Button Event Handler
+         * Handles when the compare button is clicked
+         * Input: void
+         * Returns: void
+         */
         btnCompare.setOnAction(actionEvent -> {
-            if (!btnInsertData.isSelected()) {
-                if (btnCompare.isSelected()) {
-                    tbInsert.setVisible(false);
-                    stage.setHeight(390);
-                    tbCompare.setVisible(true);
-                    btnInsertData.setDisable(true);
-                } else {
-                    stage.setHeight(330);
-                    tbCompare.setVisible(false);
-                    btnInsertData.setDisable(false);
-                }
+            if (btnCompare.isSelected()) {  //Expands the menu if the button is selected
+                stage.setWidth(950);
+                tbCompare.setVisible(true);
+            } else {    //Reverts the menu expansion if the button is NOT selected
+                stage.setWidth(800);
+                tbCompare.setVisible(false);
             }
         });
 
+        /**
+         * Insert Button Event Handler
+         * Handles when the insert button is clicked
+         * Input: void
+         * Returns: void
+         */
         btnInsertData.setOnAction(actionEvent -> {
-            if(!btnCompare.isSelected()) {
-                if (btnInsertData.isSelected()) {
-                    tbCompare.setVisible(false);
-                    btnCompare.setDisable(true);
-                    if (btnInsertSubmit.getText() != "Submit Hitter Stats") {
-                        btnInsertData.setDisable(true);
-                        stage.setHeight(390);
-                        tbInsert.setVisible(true);
-                    } else {
-                        stage.setHeight(330);
-                        tbInsert.setVisible(false);
-                    }
-                } else {
-                    stage.setHeight(330);
-                    tbInsert.setVisible(false);
-                    btnCompare.setDisable(false);
-                }
+            if (btnInsertData.isSelected()) {   //Expands the menu if the button is toggled
+                stage.setHeight(390);
+                tbInsert.setVisible(true);
+            } else {    //Reverts the menu expansion if the menu button is NOT toggled
+                stage.setHeight(330);
+                tbInsert.setVisible(false);
             }
         });
 
-        /*btnInsertSubmit.setOnAction(actionEvent -> {
-            if (btnInsertSubmit.getText() != "Submit Pitcher Stats") { //Checks to see which setting the btn is on
-                btnInsertSubmit.setText("Submit Pitcher Stats");
-            } else {
-                btnInsertSubmit.setText("Submit Hitter Stats");
-                btnInsertData.setDisable(false);
+        /**
+         * Compare Button Position Event Handler
+         * Handles when the compare button position is changed (i.e. hitter to pitcher and vice versa)
+         * Input: void
+         * Returns: void
+         */
+        btnComparePosition.setOnAction(actionEvent -> {
+            if (btnComparePosition.getValue() == "Pitcher") {   //Checks if the current comparison menu is pitcher
+                lblCompareStatOne.setPromptText(pitcher[0]);    //Sets prompt texts for each comparison textfield
+                lblCompareStatTwo.setPromptText(pitcher[1]);
+                lblCompareStatThree.setPromptText(pitcher[2]);
+                lblCompareStatFour.setPromptText(pitcher[3]);
+                lblCompareStatFive.setPromptText(pitcher[4]);
+                lblCompareStatSix.setPromptText(pitcher[5]);
+                lblCompareStatSeven.setPromptText(pitcher[6]);
+                lblCompareStatEight.setPromptText(pitcher[7]);
+                lblCompareStatNine.setPromptText(pitcher[8]);
+                lblCompareStatTen.setPromptText(pitcher[9]);
+                TTOne.setText(pitcher[0]);  //Sets tooltips for each comparison textfield
+                TTTwo.setText(pitcher[1]);
+                TTThree.setText(pitcher[2]);
+                TTFour.setText(pitcher[3]);
+                TTFive.setText(pitcher[4]);
+                TTSix.setText(pitcher[5]);
+                TTSeven.setText(pitcher[6]);
+                TTEight.setText(pitcher[7]);
+                TTNine.setText(pitcher[8]);
+                TTTen.setText(pitcher[9]);
+            } else {    //Checks if the current comparison menu is hitter
+                lblCompareStatOne.setPromptText(hitter[0]); //Sets prompt texts for each comparison textfield
+                lblCompareStatTwo.setPromptText(hitter[1]);
+                lblCompareStatThree.setPromptText(hitter[2]);
+                lblCompareStatFour.setPromptText(hitter[3]);
+                lblCompareStatFive.setPromptText(hitter[4]);
+                lblCompareStatSix.setPromptText(hitter[5]);
+                lblCompareStatSeven.setPromptText(hitter[6]);
+                lblCompareStatEight.setPromptText(hitter[7]);
+                lblCompareStatNine.setPromptText(hitter[8]);
+                lblCompareStatTen.setPromptText(hitter[9]);
+                TTOne.setText(hitter[0]);  //Sets tooltips for each comparison textfield
+                TTTwo.setText(hitter[1]);
+                TTThree.setText(hitter[2]);
+                TTFour.setText(hitter[3]);
+                TTFive.setText(hitter[4]);
+                TTSix.setText(hitter[5]);
+                TTSeven.setText(hitter[6]);
+                TTEight.setText(hitter[7]);
+                TTNine.setText(hitter[8]);
+                TTTen.setText(hitter[9]);
             }
-        });*/
+        });
 
-        scene.getAccelerators().put(KeyCombination.keyCombination("CTRL+H"), new Runnable() {
+        /**
+         * Help Shortcut Event Handler
+         * Handles when the help command shortcut is activated (CTRL+H)
+         * Input: CTRL+H
+         * Returns: void
+         */
+        scene.getAccelerators().put(KeyCombination.keyCombination("CTRL+H"), new Runnable() {   //Detects CTRL+H input
             @Override
             public void run() {
-                cmd.accessExternalSource("help");
-            }
-        });
-        scene.getAccelerators().put(KeyCombination.keyCombination("CTRL+E"), new Runnable() {
-            @Override
-            public void run() {
-                cmd.accessExternalSource("export");
+                cmd.accessExternalSource("help");   //Runs the help command, identical to help button
             }
         });
 
+        /**
+         * Export Shortcut Event Handler
+         * Handles when the export command is activated (CTRL+E)
+         * Input: void
+         * Returns: void
+         */
+        scene.getAccelerators().put(KeyCombination.keyCombination("CTRL+E"), new Runnable() {   //Detects CTRL+E input
+            @Override
+            public void run() {
+                cmd.accessExternalSource("export"); //Runs the export command, identical to export command
+            }
+        });
     }
 
-
+    /**
+     * Application Closing Event Handler
+     * Handles when the application is closing and saves the data automatically
+     * Input: void
+     * Returns: void
+     */
     @Override
     public void stop(){
-        System.out.println("Stage is closing");
-        // Save file
-        TableIterator thisThing = new TableIterator();
-        thisThing.writeToFile();
+        TableIterator thisThing = new TableIterator();  //Iterate through database/external source
+        thisThing.writeToFile();    //Write modifications to file prior to closing
     }
 
+    /**
+     * Main
+     * Handles the application's fundamental process
+     */
     public static void main(String[] args){
         launch();
     }
-
 }
