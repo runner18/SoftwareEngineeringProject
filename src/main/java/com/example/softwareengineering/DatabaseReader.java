518a7b52d24ecdf85395d4920d3c91fec93bee7d
t-class1 <t-class.1@onu.edu>
2021-11-20 13:18:30 -0500

    TableIterator and DatabaseReader fully commented

--- src/main/java/com/example/softwareengineering/DatabaseReader.java
+++ src/main/java/com/example/softwareengineering/DatabaseReader.java
@@ -5,12 +5,15 @@ import java.util.Arrays;
 
 public class DatabaseReader {
 
-    //CSV over SQL because I could only find that the paid version supports SQL until well after this was working
-    String location = "src/main/resources/com/example/softwareengineering/";
-    String battingStats = "BattingStats.csv";
-    String pitchingStats = "PitchingStats.csv";
-    static String[] allTeams;
+    String location = "src/main/resources/com/example/softwareengineering/";//The location path of the database files
+    String battingStats = "BattingStats.csv";//the name of the hitter database file
+    String pitchingStats = "PitchingStats.csv";//the name of the pitcher database file
+    static String[] allTeams;//the accessible list of all team names
 
+    /**
+     * DatabaseReader Default constructor
+     * Default constructor for the DatabaseReader class
+     */
     public DatabaseReader(){}
 
     /**
@@ -34,7 +37,7 @@ public class DatabaseReader {
     }
 
     /**
-     * GetHittersMethod
+     * GetBattersMethod
      * Translates the data from the database file into a usable array of Hitter objects
      * @param stats - A filereader object with the location of where the database file is and what it is named
      */
@@ -52,16 +55,16 @@ public class DatabaseReader {
             e.printStackTrace();//the output if there is an error
         }
 
-        Hitter[] obj = new Hitter[i];
-        for(int j =0; j < i; j++){
+        Hitter[] obj = new Hitter[i];//initializing an array of Hitter objects based on the amount of lines found earlier
+        for(int j =0; j < i; j++){//A loop to fill all of the newly created objects with database information
             obj[j] = new Hitter(j + "", "hitter", Double.parseDouble(words[j][2]), Double.parseDouble(words[j][3]), Double.parseDouble(words[j][4]), Double.parseDouble(words[j][5]), Double.parseDouble(words[j][6]), Double.parseDouble(words[j][7]), Double.parseDouble(words[j][8]), Double.parseDouble(words[j][9]), Double.parseDouble(words[j][10]));
             obj[j].setPersonTeam(words[j][1]);
             obj[j].setPersonName(words[j][0]);
         }
-        TableIterator hitter = new TableIterator();
-        hitter.setHitterInformation(words);
-        //
-        return obj;
+        TableIterator hitter = new TableIterator();//Creating a new TableIterator
+        hitter.setHitterInformation(words);//Setting the initial information for use when saving or exporting data
+
+        return obj;//returning the finished Hitter array
     }
 
     /**
@@ -70,39 +73,29 @@ public class DatabaseReader {
      * @param stats - A filereader object with the location of where the database file is and what it is named
      */
     private Pitcher[] getPitchers(FileReader stats){
-        String line;
-        String splitBy = ",";
-        BufferedReader br = new BufferedReader(stats);
-        int i = 0;
-        String[][] words = new String[100][];
-        try {
-            while ((line = br.readLine()) != null) {
-                words[i] = line.split(splitBy);
-                i++;
+        String line;//A string to be used for temporarily holding information from the database until it can be formatted for an array
+        String splitBy = ",";//the splitting point for different pieces of information from the database
+        BufferedReader br = new BufferedReader(stats);//creating a buffered reader for reading in the FileReader stats
+        int i = 0;//initialising the counter for tracking the size of the following array
+        String[][] words = new String[100][];//initialising a 2 dimensional String array for storing player information from the file
+        try {//ensuring the program does not fail upon a bad read of the database file
+            while ((line = br.readLine()) != null) {//making sure there is more data to be read
+                words[i++] = line.split(splitBy);//separating the information for each player into an array and then incrementing the counter
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception e) {//the result if there is an error from the database file
+            e.printStackTrace();//the output if there is an error
         }
-        Pitcher[] obj = new Pitcher[i];
-        for(int j = 0; j < i; j++) {
-            obj[j] = new Pitcher(j + "",
-                    "pitcher",
-                    Double.parseDouble(words[j][2]) + 0,
-                    Double.parseDouble(words[j][3]) + 0,
-                    Double.parseDouble(words[j][4]) + 0,
-                    Double.parseDouble(words[j][5]) + 0,
-                    Double.parseDouble(words[j][6]) + 0,
-                    Double.parseDouble(words[j][7]) + 0,
-                    Double.parseDouble(words[j][8]) + 0,
-                    Double.parseDouble(words[j][9]) + 0,
-                    Double.parseDouble(words[j][10]) + 0,
-                    Double.parseDouble(words[j][11]) + 0);
+
+        Pitcher[] obj = new Pitcher[i];//initializing an array of Pitcher objects based on the amount of lines found earlier
+        for(int j = 0; j < i; j++) {//A loop to fill all of the newly created objects with database information
+            obj[j] = new Pitcher(j + "", "pitcher", Double.parseDouble(words[j][2]), Double.parseDouble(words[j][3]), Double.parseDouble(words[j][4]), Double.parseDouble(words[j][5]), Double.parseDouble(words[j][6]), Double.parseDouble(words[j][7]), Double.parseDouble(words[j][8]), Double.parseDouble(words[j][9]), Double.parseDouble(words[j][10]), Double.parseDouble(words[j][11]));
             obj[j].setPersonName(words[j][0]);
             obj[j].setPersonTeam(words[j][1]);
         }
-        TableIterator pitcher = new TableIterator();
-        pitcher.setPitcherInformation(words);
-        return obj;
+        TableIterator pitcher = new TableIterator();//Creating a new TableIterator
+        pitcher.setPitcherInformation(words);//Setting the initial information for use when saving or exporting data
+
+        return obj;//returning the finished Pitcher array
 
 
     }
@@ -123,45 +116,49 @@ public class DatabaseReader {
                     teamCheck++;
                 }
             }
-            if(teamCheck == 0){
-                for(int j = 0; j < teams.length; j++){
-                    if(teams[j].equals("")){
-                        teams[j] = hitters[i].getPersonTeam();
-                        j = teams.length;
+            if(teamCheck == 0){//checking the Flag int to see if this team has been used before
+                for(int j = 0; j < teams.length; j++){//looping through all teams to find the first empty slot
+                    if(teams[j].equals("")){//finding the placement of the first empty index
+                        teams[j] = hitters[i].getPersonTeam();//setting the last player's team to the open slot
+                        j = teams.length;//leaving the loop
                     }
                 }
             }
         }
-        for(int i = 0; i < pitchers.length; i++){
-            int teamCheck = 0;
-            for(int j = 0; j < teams.length; j++){
-                if(teams[j].length() > 0 && teams[j].equals(pitchers[i].getPersonTeam())){
-                    teamCheck++;
+        for(int i = 0; i < pitchers.length; i++){//looping through all hitters available
+            int teamCheck = 0;//Flag int for if this teams has been found before
+            for(int j = 0; j < teams.length; j++){//loop for all team names
+                if(teams[j].length() > 0 && teams[j].equals(pitchers[i].getPersonTeam())){//checking all teams already found
+                    teamCheck++;//increasing the Flag int
                 }
             }
-            if(teamCheck == 0){
-                for(int j = 0; j < teams.length; j++){
-                    if(teams[j].equals("")){
-                        teams[j] = pitchers[i].getPersonTeam();
-                        j = teams.length;
+            if(teamCheck == 0){//checking the Flag int to see if this team has been used before
+                for(int j = 0; j < teams.length; j++){//looping through all teams to find the first empty slot
+                    if(teams[j].equals("")){//finding the placement of the first empty index
+                        teams[j] = pitchers[i].getPersonTeam();//setting the last player's team to the open slot
+                        j = teams.length;//leaving the loop
                     }
                 }
             }
         }
-        int totalLength = 1;
-        for(int i = 0; i < teams.length; i++) {
-            if (!teams[i].equals("")) {
-                totalLength++;
+        int totalLength = 1;//preparing to create an array of the actual number of teams, with an extra for setting the DisplayAll option
+        for(int i = 0; i < teams.length; i++) {//checking all indexes for team names
+            if (!teams[i].equals("")) {//finding each index of team names
+                totalLength++;//increasing the count for what the actual number of team names is
             }
         }
-        String[] finalTeams = new String[totalLength];
-        finalTeams[0] = "All";
-        for(int i = 1; i < totalLength; i++){
-            finalTeams[i] = teams[i-1];
+        String[] finalTeams = new String[totalLength];//creating the array with the correct number of teams
+        finalTeams[0] = "All";//Initializing the DisplayAll option
+        for(int i = 1; i < totalLength; i++){//loop for setting all indexes of the array
+            finalTeams[i] = teams[i-1];//offset to include the All option
         }
-        allTeams = finalTeams;
+        allTeams = finalTeams;//setting the global variable for when it needs to be called upon later
     }
 
+    /**
+     * GetTeamsMethod
+     * Return a string array of all team names
+     */
     public static String[] getTeams(){
         return allTeams;
     }
--- src/main/java/com/example/softwareengineering/TableIterator.java
+++ src/main/java/com/example/softwareengineering/TableIterator.java
@@ -4,124 +4,173 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 
 public class TableIterator {
-    private StringBuilder sb = new StringBuilder();
-    private static String[][] hitterInformation;
-    private static String[][] pitcherInformation;
-    private static int hitIndex = 0;
-    private static int pitchIndex = 0;
-    private String directory = "src/main/resources/com/example/softwareengineering/";
 
+    private StringBuilder sb = new StringBuilder();//String Builder object for writing to files
+    private static String[][] hitterInformation;//An array of all hitter information
+    private static String[][] pitcherInformation;//An array of all pitcher information
+    private static int hitIndex = 0;//Safety index in the event that null data is found in the array of hitters
+    private static int pitchIndex = 0;//Safety index in the event that null data is found in the array of pitchers
+    private String directory = "src/main/resources/com/example/softwareengineering/";//The location path of the database files
+
+    /**
+     * WriteToFileMethod
+     * The public method used to start the saving process for both hitters and pitchers in the proper manner
+     */
     public void writeToFile(){
-        sb = new StringBuilder();
-        hitWriting(directory + "BattingStats.csv");
-        sb = new StringBuilder();
-        pitchWriting(directory + "PitchingStats.csv");
+        sb = new StringBuilder();//resets the String Builder object
+        hitWriting(directory + "BattingStats.csv");//Calling the hitWriting method using the appropriate location
+        sb = new StringBuilder();//resets the String Builder object
+        pitchWriting(directory + "PitchingStats.csv");//Calling the pitchWriting method using the appropriate location
     }
 
+    /**
+     * ExportFileMethod
+     * Returns the full array of batters or pitchers from the database
+     * @param location - file directory path as specified by the user
+     */
     public void exportFile (String location){
-        for(int i = location.length() - 1; i > 0; i--){
-            if(location.charAt(i) == '\\'){
-                location = location.substring(0, i);
-                i = 0;
+        for(int i = location.length() - 1; i > 0; i--){//Loop through all characters of the string parameter starting from the back
+            if(location.charAt(i) == '\\'){//Find the last instance of the \ character
+                location = location.substring(0, i);//Use the string with everything up to the last \ character
+                i = 0;//exit the loop
             }
         }
-        sb = new StringBuilder();
-        hitWriting(location + "\\BattingStats.csv");
-        sb = new StringBuilder();
-        pitchWriting(location + "\\PitchingStats.csv");
+        sb = new StringBuilder();//reset the stringBuilder object
+        hitWriting(location + "\\BattingStats.csv");//Access the hitWriting method with the new location and what will be the name of the written file
+        sb = new StringBuilder();//reset the stringBuilder object
+        pitchWriting(location + "\\PitchingStats.csv");//Access the pitchWriting method with the new location and what will be the name of the written file
     }
 
+    /**
+     * SetHitterInformationMethod
+     * Uses the passed information to set the global static hitterInformation
+     * @param hitterInformation - the information on all hitters as passed from the accessing object
+     */
     public void setHitterInformation(String[][] hitterInformation){
-        this.hitterInformation = hitterInformation;
-        for(int i = 0; i < hitterInformation.length; i++){
-            try {
-                if (!hitterInformation[i][0].equals(null)) {
-                    hitIndex++;
+        this.hitterInformation = hitterInformation;//Setting the global variable to the passed information
+        for(int i = 0; i < hitterInformation.length; i++){//looping through the array to find the correct length
+            try {//watching for mullInformation errors
+                if (!hitterInformation[i][0].equals(null)) {//watching for information that is not the end of the array
+                    hitIndex++;//increasing the global variable index of the hitterInformation array
                 }
-            }catch(Exception e){}
+            }catch(Exception e){e.printStackTrace();}//printing out errors found if needed
         }
     }
 
+    /**
+     * SetPitcherInformationMethod
+     * Uses the passed information to set the global static pitcherInformation
+     * @param pitcherInformation - the information on all pitchers as passed from the accessing object
+     */
     public void setPitcherInformation(String[][] pitcherInformation){
-        this.pitcherInformation = pitcherInformation;
-        for(int i = 0; i < pitcherInformation.length; i++){
-            try {
-                if (!pitcherInformation[i][0].equals(null)) {
-                    pitchIndex++;
+        this.pitcherInformation = pitcherInformation;//Setting the global variable to the passed information
+        for(int i = 0; i < pitcherInformation.length; i++){//looping through the array to find the correct length
+            try {//watching for mullInformation errors
+                if (!pitcherInformation[i][0].equals(null)) {//watching for information that is not the end of the array
+                    pitchIndex++;//increasing the global variable index of the pitcherInformation array
                 }
             }catch(Exception e){}
         }
     }
 
+    /**
+     * InsertInfoMethod
+     * Places a new player into the correct array
+     * @param position - the name of the correct array for the player to be placed into
+     * @param input - the information about the newly created player
+     */
     public void insertInfo(String position, String[] input){
-        if (position.equals("pitcher")) {
-            pitcherInformation[pitchIndex++] = input;
+        if (position.equals("pitcher")) {//checking which type of player was created
+            pitcherInformation[pitchIndex++] = input;//inserting the new data and increasing the index to match
         }
         else{
-            hitterInformation[hitIndex++] = input;
+            hitterInformation[hitIndex++] = input;//inserting the new data and increasing the index to match
         }
     }
 
+    /**
+     * LoopHitMethod
+     * Updates the global string builder object to print out all of the hitter information
+     */
     private void loopHit(){
-        for(int i = 0; i < hitterInformation.length; i++){
-            for(int j = 0; j < 11; j++){
-                try {
-                    if (!hitterInformation[i][j].equals(null)) {
-                        sb.append(hitterInformation[i][j] + ',');
-                    } else {
+        for(int i = 0; i < hitterInformation.length; i++){//iterate through the hitterInformation array
+            for(int j = 0; j < 11; j++){//checking for each stat in the second dimension of the hitterInformation array
+                try {//checking for nullInformation errors
+                    if (!hitterInformation[i][j].equals(null)) {//checking for information in the specific
+                        sb.append(hitterInformation[i][j] + ',');//Adding the information to the StringBuilder for use later as well as a comma for separation
+                    } else {//Exits the current loop
                         i = hitterInformation.length;
                         j = 11;
                     }
-                }catch(Exception e){}
+                }catch(Exception e){e.printStackTrace();}//print out any errors if needed
             }
-            try {
-                if (!hitterInformation[i][0].equals(null)) {
-                    sb.append('\n');
+            try {//watching for nullInformation errors
+                if (!hitterInformation[i][0].equals(null)) {//Looking for a nonNull point
+                    sb.append('\n');//Adding a breakline to the StringBuilder to help format the upcoming printed file
                 }
-            }catch(Exception e){}
+            }catch(Exception e){e.printStackTrace();}//print out any errors if needed
         }
     }
 
+    /**
+     * LoopPitchMethod
+     * Updates the global string builder object to print out all of the pitcher information
+     */
     private void loopPitch(){
-        for(int i = 0; i < pitcherInformation.length; i++){
-            for(int j = 0; j < 12; j++){
-                try {
-                    if (!pitcherInformation[i][j].equals(null)) {
-                        sb.append(pitcherInformation[i][j] + ',');
-                    } else {
+        for(int i = 0; i < pitcherInformation.length; i++){//iterate through the pitcherInformation array
+            for(int j = 0; j < 12; j++){//checking for each stat in the second dimension of the pitcherInformation array
+                try {//checking for nullInformation errors
+                    if (!pitcherInformation[i][j].equals(null)) {//checking for information in the specific
+                        sb.append(pitcherInformation[i][j] + ',');//Adding the information to the StringBuilder for use later as well as a comma for separation
+                    } else {//Exits the current loop
                         i = pitcherInformation.length;
                         j = 11;
                     }
-                }catch(Exception e){}
+                }catch(Exception e){e.printStackTrace();}//print out any errors if needed
             }
-            try {
-                if (!pitcherInformation[i][0].equals(null)) {
-                    sb.append('\n');
+            try {//watching for nullInformation errors
+                if (!pitcherInformation[i][0].equals(null)) {//Looking for a nonNull point
+                    sb.append('\n');//Adding a breakline to the StringBuilder to help format the upcoming printed file
                 }
-            } catch (Exception e){}
+            } catch (Exception e){e.printStackTrace();}//print out any errors if needed
         }
     }
 
+    /**
+     * HitWritingMethod
+     * Calls the loopHit and writing methods in a specific order to ensure events happen sequentially
+     * @param name - the name of the path to be used in writing the file
+     */
     private void hitWriting(String name){
         loopHit();
         writing(name);
     }
 
+    /**
+     * PitchWritingMethod
+     * Calls the loopPitch and writing methods in a specific order to ensure events happen sequentially
+     * @param name - the name of the path to be used in writing the file
+     */
     private void pitchWriting(String name){
         loopPitch();
         writing(name);
     }
 
+    /**
+     * WritingMethod
+     * Uses the given name variable to be able to write a given file in a specific directory in a user's file system
+     * @param name - the name of the path to be used in writing the file
+     */
     private void writing(String name) {
-        try{
-            FileWriter file = new FileWriter(name);
-            try (PrintWriter writer = new PrintWriter(file)) {
-                writer.write(sb.toString());
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+        try{//watching for any kind of error that may be involved with file writing
+            FileWriter file = new FileWriter(name);//creating a new FileWriter object based on the parameter
+            try (PrintWriter writer = new PrintWriter(file)) {//Attempting to create a PrintWriter based on the FileWriter
+                writer.write(sb.toString());//Using the StringBuilder object to write with the PrintWriter object
+            } catch (Exception e) {//catching any found errors
+                System.out.println(e.getMessage());//Printing out the message from the error that was found
             }
-        }catch(Exception e) {
-            System.out.println(e.getMessage());
+        }catch(Exception e) {//catching any found errors
+            System.out.println(e.getMessage());//Printing out the message from the error that was found
         }
     }
 }
